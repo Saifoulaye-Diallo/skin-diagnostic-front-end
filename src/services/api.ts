b4481d2b18f7fa0decca8b199cd0b6a53130e2a5
Saifon <79662118+Saifoulaye-Diallo@users.noreply.github.com>
2025-04-04 01:09:06 -0400

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
-
+const baseURL = import.meta.env.VITE_API_URL;
+console.log("✅ Axios baseURL VITE_API_URL :", baseURL);
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
-console.log("✅ Axios baseURL VITE_API_URL :", import.meta.env.VITE_API_URL);
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
