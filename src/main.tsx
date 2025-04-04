a275bec0c37daeeb1523f86e4e0065ac9bd21f0b
Saifon <79662118+Saifoulaye-Diallo@users.noreply.github.com>
2025-04-04 01:21:25 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
+console.log("Toutes les variables Vite :", import.meta.env);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
