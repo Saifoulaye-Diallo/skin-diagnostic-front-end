54cc79fd04bd729e7ccfe7bf5454b6b6f06ec57a
Saifon <79662118+Saifoulaye-Diallo@users.noreply.github.com>
2025-04-05 00:30:50 -0400

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { LogIn, User, Lock, ArrowRight } from 'lucide-react';
